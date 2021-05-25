d119f1332b0756938591f7c8270bf931c0bc3b8b
Sayan Roy <sayanr_87@yahoo.co.in>
2021-05-25 18:09:56 +0530

    Get one record method implemented

--- src/controller.fs.js
+++ src/controller.fs.js
@@ -1,6 +1,32 @@
 var fs = require('fs'),
-dataPath = '../assets/data/records.json';
+dataPath = './assets/data/records.json';
 
 exports.getAllRecord = function(req, res) {
-    res.send('Test successful');
+    fs.readFile(dataPath, 'utf8', (err, response) => {
+        if (err) {
+            res.status(406).send(err);
+        }
+        res.send(response);
+    });
 };
+
+exports.getOneRecord = function(req, res) {
+    fs.readFile(dataPath, 'utf8', (err, response) => {
+        if (err) {
+            res.status(406).send(err);
+        }
+        var recId = req.params.recId;
+        response = JSON.parse(response);
+        var selectedRec;
+        response.forEach((e, i) => {
+            if (e.id == recId) {
+                selectedRec = e;
+            }
+        });
+        if (selectedRec) {
+            res.send(selectedRec);
+        } else {
+            res.send('No records found');
+        }
+    });
+}
--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ module.exports = function(app, express) {
     var controller = require('./controller.fs');
 // Routing paths start
     app.route('/get-all-records').get(controller.getAllRecord);
-
+    app.route('/get-one-record/:recId').get(controller.getOneRecord);
 // Routing path ends
     
 }
